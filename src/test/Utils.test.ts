6efa117007e38bae5019430878f93804769401e5
extasy44 <seoheejun@gmail.com>
2020-11-25 23:55:58 +1100

    jest hooks

--- src/test/Utils.test.ts
+++ src/test/Utils.test.ts
@@ -1,6 +1,14 @@
 import { Utils } from '../app/Utils';
 
 describe('Utils test suite', () => {
+  beforeEach(() => {
+    console.log('before each');
+  });
+
+  beforeAll(() => {
+    console.log('before all');
+  });
+
   test('first test', () => {
     const result = Utils.toUpperCase('abc');
     expect(result).toBe('ABC');
@@ -11,5 +19,18 @@ describe('Utils test suite', () => {
     expect(parsedUrl.href).toBe('http://localhost:8080/login');
     expect(parsedUrl.port).toBe('8080');
     expect(parsedUrl.protocol).toBe('http:');
+    expect(parsedUrl.query).toEqual({});
+  });
+
+  test('parse URL with query', () => {
+    const parsedUrl = Utils.parseUrl(
+      'http://localhost:8080/login?user=user&password=pass'
+    );
+    const expectedQuery = {
+      user: 'user',
+      password: 'pass',
+    };
+    expect(parsedUrl.query).toEqual(expectedQuery);
+    expect(expectedQuery).toBe(expectedQuery);
   });
 });
